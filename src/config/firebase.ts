a16688f95dfc2067b31100d13e46a9426b56c06c
EEC14 <61870511+EEC14@users.noreply.github.com>
2024-12-11 16:01:16 +0100

    Google analytics

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDseSpx5tJ1mpF0S0vFV2dV-S9kWtI2jb8",
@@ -15,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+const analytics = getAnalytics(app);
