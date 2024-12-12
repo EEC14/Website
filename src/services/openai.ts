a723ae117b16abb4d072b885f2b4df9c3d4e20b8
EEC14 <61870511+EEC14@users.noreply.github.com>
2024-12-12 09:46:38 +0100

    Update openai.ts

--- src/services/openai.ts
+++ src/services/openai.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 import { UserProfile } from "../services/firebase/firestore";
-
+import { useAuth } from "../contexts/AuthContext";
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true,
