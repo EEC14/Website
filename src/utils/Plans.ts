189abedc575a68ac3d8cc164ba1bfc4180d0dc7b
EEC14 <61870511+EEC14@users.noreply.github.com>
2025-01-09 13:27:21 +0100

    Update Plans.ts

--- src/utils/Plans.ts
+++ src/utils/Plans.ts
@@ -21,4 +21,26 @@ export const Plans = {
       "Diet plans generator",
     ],
   },
+  ProYearly: {
+    link: "https://buy.stripe.com/14k8wSctY6vwbgk3cg",
+    priceId: "price_1QfKhvGuEHc4ZQvQQJ9GVK8Q", // Replace with your actual Stripe Price ID
+    name: "Pro - Yearly",
+    price: 30,
+    features: [
+      "Unlimited chatbot access",
+      "Daily health tips",
+    ],
+  },
+  DeluxeYearly: {
+    link: " https://buy.stripe.com/28o00m1Pkf22848003",
+    priceId:
+      "price_1QfKiFGuEHc4ZQvQCfFAmkdn", // Replace with your actual Stripe Price ID
+    name: "Deluxe - Yearly",
+    price: 45,
+    features: [
+      "All Pro features",
+      "Workout plans generator",
+      "Diet plans generator",
+    ],
+  },
 };
